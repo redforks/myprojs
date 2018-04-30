a2970ba2b3483a35a9f750aef2ce9cc03667ec6e
Red Forks <redforks@gmail.com>
2018-04-30 15:46:16 +0800

    Import myprojs.py

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # myprojs
+
+`myprojs` is a python script manages git directories.
+
+If you like me have many projects use `git` as source repository, you'll known the pain of forget to `push` your source code.
+
+Use `myprojs` to cure the pain:
+
+```bash
+myprojs.py --need-push -- git push
+```
+
+## Basic Usage
+
+To list your project directories:
+
+```bash
+myprojs.py
+```
+
+To list dirty projects, i.e. contains changes not committed.
+
+```bash
+myprojs.py --dirty
+```
+
+To list not pushed projects:
+
+```bash
+myprojs.py --need-push
+```
+
+Execute command on your projects, such as unit tests:
+
+```bash
+myprojs.py -- jest
+```
+
+`jest` or other unit tests tool, will execute at each project's directory.
+
+## Project Type
+
+Project directory list stored in `~/.myprojs` directory, file is
+a text file, each line is a project directory. Ignore lines starts
+with `'#'`. Each file defines a project type, such as:
+
+```
+cat ~/.myprojs/chat
+
+~/chat/server
+~/chat/client
+~/chat/scripts
+
+cat ~/myprojs/bbs
+
+~/bbs/list
+~/bbs/article
+~/bbs/backend
+
+```
+
+Define your projects, run command on specified project group:
+
+```
+myprojs.py --project-type chat --dirty --push
+```
+
+A special project type is `'all'`, include project directory of all
+project types. `'all'` is default.
+
+Duplicate directories are ignored, especially useful for `all` project type.
+
+## Install
+
+`myproj.py` is a simple python script, download and mark it as executable, put it into one of `PATH` directory.
---
+++ myprojs.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import os.path
+from subprocess import run, PIPE
+
+
+parser = argparse.ArgumentParser(description="Manage my project directories")
+parser.add_argument("--dirty", help="filter out dirty work directories",
+                    action='store_true')
+parser.add_argument("--need-push", help="filter out directory need git push",
+                    action='store_true', dest='needPush')
+parser.add_argument("--project-type",
+                    help='Select project type, project type defined at'
+                    " ~/.myprojs directory, default to 'all', combines all "
+                    'project files.',
+                    default='all')
+parser.add_argument('Command', nargs="?",
+                    help="Command run on each project directory, " +
+                    "default to 'pwd'", default='pwd')
+
+args = parser.parse_args()
+
+
+def isDirty(p):
+    """returns true if project p is dirty, i.e. contains changes not commit
+    to git.
+    """
+    rv = run(["git", "status", "-s"], cwd=p, stdout=PIPE)
+    if rv.returncode != 0:
+        return False
+    return bool(rv.stdout)
+
+
+def needPush(p):
+    """returns true if directory contains git commits not pushed.
+    """
+    rv = run(["git", "status", "-sb"], cwd=p, stdout=PIPE)
+    if rv.returncode != 0:
+        return False
+    return 'ahead ' in str(rv.stdout)
+
+
+def forEach(cmd, projs):
+    """
+    forEach execute shell command in each src directory.
+    """
+    failedDirs = []
+    for p in projs:
+        cp = run(cmd, cwd=p, shell=True)
+        if cp.returncode != 0:
+            failedDirs.append(p)
+
+    if failedDirs:
+        print("failed dirs:", "\n".join(failedDirs))
+
+
+proj_set_dir = os.path.expanduser('~/.myprojs')
+
+
+def load_projects(proj_set):
+    """Load projects by project set.
+
+    Project set is a file located at ~/.myprojs.
+
+    Empty and started with '#' lines are ignored, each line should be a
+    absolute path point to a git directory.
+
+    Directory not exist are exclude, and displayed as a warning.
+
+    Directory path should be absolute, support '~' as home directory.
+
+    :param proj_set: project set name
+    :returns: returns list of project directories.
+    """
+
+    def dir_exist(d):
+        if not os.path.exists(d):
+            print(d, 'not exist')
+            return False
+        return True
+
+    fn = os.path.join(proj_set_dir, proj_set)
+    with open(fn) as f:
+        lines = f.readlines()
+
+    lines = [l.strip() for l in lines]
+    lines = [l for l in lines if l and not l.startswith('#')]
+    lines = list(set(lines))
+    lines = [os.path.expanduser(l) for l in lines]
+    return remove_dup(filter(dir_exist, lines))
+
+
+def remove_dup(strs):
+    """Remove string array duplicate items.
+
+    :param strs: list of string
+    :returns: sorted strings with duplicate items removed.
+    """
+    r = list(set(strs))
+    r.sort()
+    return r
+
+
+def load_all():
+    """Load all project files, and merge them all.
+
+    Ignore hidden files, (files start with '.').
+
+    Duplicate projects are merged.
+
+    : returns: return list of all project directories.
+    """
+    dirs = []
+    for ps in os.listdir(proj_set_dir):
+        if not ps.startswith('.'):
+            dirs.extend(load_projects(ps))
+    return remove_dup(dirs)
+
+
+if __name__ == '__main__':
+    if args.project_type == 'all':
+        selected = load_all()
+    else:
+        selected = load_projects(args.project_type)
+
+    if args.dirty:
+        selected = [x for x in selected if isDirty(x)]
+
+    if args.needPush:
+        selected = [x for x in selected if needPush(x)]
+
+    forEach(args.Command, selected)
